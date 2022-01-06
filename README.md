d1e3349682a9a10328a6e8645625854849ca9961
Manuj Sankrit <msankrit8055@gmail.com>
2022-01-06 22:34:51 +0530

    Completed expanding cards mini project

--- README.md
+++
@@ -1,2 +0,0 @@
-# mini_projects_HCJ
-Mini html, css and js based projects
---
+++ proj1/script.js
@@ -0,0 +1,12 @@
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeAllActiveClasses();
+    panel.classList.add('active');
+  });
+});
+
+const removeAllActiveClasses = () => {
+  panels.forEach((panel) => panel.classList.remove('active'));
+};
